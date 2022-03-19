33c629e4ec61fe08e8f3ff4787f768cd11c6e52e
ahmedabzk <ahmed.abzk@gmail.com>
2022-03-19 20:29:51 +0300

    implemented the adding video functionality

---
+++ lib/views/screens/add_video_screen.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_types_as_parameter_names, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/views/screens/confirm_screen.dart';
+
+class AddVideoScreen extends StatelessWidget {
+  const AddVideoScreen({Key? key}) : super(key: key);
+
+  pickVideo(ImageSource src, BuildContext context) async {
+    final video = await ImagePicker().pickVideo(source: src);
+    if (video != null) {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => ConfirmScreen())
+      );
+    }
+  }
+
+  ShowOptionsDialog(BuildContext, context) {
+    return showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+              children: [
+                SimpleDialogOption(
+                  onPressed: () => pickVideo(ImageSource.gallery, context),
+                  child: Row(
+                    children: [
+                      Icon(Icons.image),
+                      Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: Text(
+                          'Gallary',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () => pickVideo(ImageSource.camera, context),
+                  child: Row(
+                    children: [
+                      Icon(Icons.camera_alt),
+                      Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: Text(
+                          'Camera',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SimpleDialogOption(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Row(
+                    children: [
+                      Icon(Icons.cancel),
+                      Padding(
+                        padding: const EdgeInsets.all(7.0),
+                        child: Text(
+                          'Cancel',
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: InkWell(
+          onTap: () => ShowOptionsDialog(context, context),
+          child: Container(
+            width: 145,
+            height: 45,
+            decoration: BoxDecoration(color: buttonColour),
+            child: Center(
+              child: Text(
+                'Add Video',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
